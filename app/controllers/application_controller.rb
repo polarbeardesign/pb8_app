720ee02623fb6b454e40356c14c1fff6c37e39d2
Polar Bear Design <pbd@PBD-MBP.local>
2025-07-01 17:02:24 -0500

    updates to menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def redirect_authenticated_user
+    if authenticated
+      redirect_to weekly_summary_path
+    end
+  end
+  
 end
